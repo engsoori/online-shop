9d345cbdffc9a344ab147ad7cad18b3b852442a4
zahrasoori <www.zahrask696@gmail.com>
2025-06-02 18:01:27 +0330

    general bluprints

---
+++ blueprintes/general.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+app = Blueprint("general", __name__)
+@app.route('/')
+
+def hello_world():
+    return 'this is main page'
--- shop.py
+++ shop.py
@@ -1,9 +1,8 @@
 from flask import Flask
-
+from blueprintes.general import app as general
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.register_blueprint(general)
+
 if __name__ == '__main__':
     app.run()
 
