664c3c0cbc6aac51a83c4a431255d26232d3ed31
zahrasoori <www.zahrask696@gmail.com>
2025-06-07 18:47:03 +0330

    admin login fix and flask-wtf setup

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from blueprintes.admin import app as admin
 from blueprintes.general import  general
@@ -18,6 +19,7 @@ app.secret_key = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI']=config.SQLALCHEMY_DATABASE_URI
 extensions.db.init_app(app)
 
+Csrf=CSRFProtect(app)
 
 with app.app_context():
     extensions.db.create_all()
